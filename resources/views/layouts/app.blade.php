7abf5d9fc60eb8f7b834ff20c991c75ef834bf87
MCyunpeng98 <84106749@qq.com>
2018-10-10 16:51:35 +0800

    个人资料配置

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,10 +17,11 @@
 <body>
 <div id="app" class="{{ route_class() }}-page">
 
+
     @include('layouts._header')
 
     <div class="container">
-
+        @include('layouts._message')
         @yield('content')
 
     </div>
