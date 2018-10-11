7065c5c8a1d9376c28251e64dcae4ec750d96a50
MCyunpeng98 <84106749@qq.com>
2018-10-11 10:24:23 +0800

    用户可以上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
@@ -16,8 +17,15 @@ class UsersController extends Controller
         return view('users.edit',compact('user'));
     }
 
-    public function update(UserRequest $request,User $user){
-        $user->update($request->all());
+    public function update(UserRequest $request,User $user,ImageUploadHandler $upload){
+        $date = $request->all();
+        if ($request->avatar){
+            $result = $upload->save($request->avatar,'avatars',$user->id,362);
+            if ($result) {
+                $date['avatar'] = $result['path'];
+            }
+        }
+        $user->update($date);
         return redirect()->route('users.show',$user->id)->with('success','个人资料修改成功');
     }
 }
