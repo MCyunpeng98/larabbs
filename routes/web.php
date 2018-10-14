25c6a9cce79762777602f9decf4f81a261b9b728
MCyunpeng98 <84106749@qq.com>
2018-10-14 16:13:19 +0800

    允许用户帖子上传图片

--- routes/web.php
+++ routes/web.php
@@ -31,8 +31,9 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 //个人页面
 Route::resource('users','UsersController',['only' => ['show','update','edit']]);
 
-//Route::resource('projects', 'ProjectsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 
 //话题分类
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
